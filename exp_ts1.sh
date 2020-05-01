ccf32c6cf4ee91862314c0210e5a90799f6b2927
Chameleon Cloud User <cc@straggler-init.novalocal>
2020-05-01 02:48:53 +0000

    c

--- exp_ts1.sh
+++ exp_ts1.sh
@@ -2,7 +2,8 @@
 
 id="6343048076"
 data_path='/Users/yiding2012/Desktop/sherlock/timeseriesspecsixtotal/'
-out="test"
+data_path='/home/cc/timeseries1total/'
+out="out1"
 
 mkdir -p $out
 mkdir -p "$out/res_ts"
@@ -33,16 +34,16 @@ mkdir -p "$out/res_agg"
 
 trap "exit" INT
 
-cd timeseriesspecsixtotal
+cd ../timeseries1total
 
 for d in * ; do
     echo "$d"
-    cd ..
-    python run_ts.py --data_path=$data_path --jobid=$d --delta=0.3 --pt=0.04 --tail=0.9 --rs=42 --out=$out
-    cd timeseriesspecsixtotal
+    cd ../sherlock
+    python run_ts.py --data_path=$data_path --jobid=$d --delta=0.6 --pt=0.04 --tail=0.9 --rs=42 --out=$out
+    cd ../timeseries1total
 done
 
-cd ..
+
 
 # python run_overall_timeseries.py --data_path=$data_path --jobid=$id --delta=0.3 --tail=0.9 --pt=0.2 --rs=42 --out=$out
 
